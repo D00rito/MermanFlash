6ce9ea006f8db8ef38d7f822901a0505883c9519
D00rito <123dvss@gmail.com>
2024-10-31 15:29:27 -0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1730405452,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/juego de nave 2,1/fondo-sheet0.png",
+		"images/juego de nave 2,1/nombre-sheet0.png",
+		"images/juego de nave 2,1/fondo2-sheet0.png",
+		"images/juego de nave 2,1/pajaro-sheet0.png",
+		"images/juego de nave 2,1/algainf-sheet0.png",
+		"images/juego de nave 2,1/algasup-sheet0.png",
+		"images/juego de nave 2,1/sprite2-sheet0.png",
+		"images/juego de nave 2,1/piso-sheet0.png",
+		"images/juego de nave 2,1/logoeco-sheet0.png",
+		"images/juego de nave 2,1/logocardenas-sheet0.png",
+		"images/juego de nave 2,1/sprite-sheet0.png",
+		"media/escritorio/sonidojuego.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
